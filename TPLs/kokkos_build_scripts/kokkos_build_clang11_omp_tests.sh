2419ba7e07cdba1b9a970f0e3ce65107014d1fc7
Rishi Ganeriwala <Rishi@Rishis-MacBook.local>
2021-06-30 21:52:24 -0700

    Create separate folder for various kokkos build scripts.

---
+++ TPLs/kokkos_build_scripts/kokkos_build_clang11_omp_tests.sh
@@ -0,0 +1,21 @@
+# command from https://github.com/kokkos/kokkos/blob/master/BUILD.md
+
+cmake \
+  -DCMAKE_CXX_COMPILER=/opt/local/bin/clang++-mp-11 \
+  -DKokkos_CXX_STANDARD="17" \
+  -DCMAKE_INSTALL_PREFIX="kokkos_install" \
+  -DKokkos_ENABLE_SERIAL=On \
+  -DKokkos_ENABLE_OPENMP=On \
+  -DKokkos_ENABLE_TESTS=On \
+  -DKokkos_ENABLE_EXAMPLES=On \
+  ..
+#  -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=On \
+#  -DKokkos_ENABLE_TESTS=On \
+#  -DKokkos_ENABLE_EXAMPLES=On 
+#  -DCMAKE_INSTALL_PREFIX=$path_to_install \
+#  -DKokkos_ENABLE_HWLOC=On \
+#  -DKokkos_HWLOC_DIR=$path_to_hwloc
+#  -DCMAKE_CXX_COMPILER=$(pwd)/../bin/nvcc_wrapper \
+#  -DCMAKE_CXX_COMPILER=g++ \
+
+make install
