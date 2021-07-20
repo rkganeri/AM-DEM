2d70f78bfdccd736e81d7ce1e1a6b26a4c927f44
Rishi Ganeriwala <Rishi@Rishis-MacBook.local>
2021-07-19 21:59:00 -0700

    Move global settings parameter setting to hpp file for clarity. Also fix to make the particles test appear as a test when running make test

--- src/global_settings.cpp
+++ src/global_settings.cpp
@@ -12,21 +12,10 @@ namespace amdem {
 // N.B. These settings are consistent with the original Fortran code PowderDepositionKhairallah.F90
 // which was used in the paper https://doi.org/10.1007/s10035-016-0626-0
 GlobalSettings::GlobalSettings(const int num_particles, const double mean_rad, const double stdev_rad) 
-    : length_(0.5e-03),       // m
-      width_(1.0e-03),
-      height_(3.0e-03),
-      mean_rad_(mean_rad),
+    : mean_rad_(mean_rad),
       stdev_rad_(stdev_rad),
-      min_rad_(4.0e-06), 
-      max_rad_(23.0e-06),
-      num_particles_(num_particles),
-      t_start_(0.0),          // s
-      t_end_(0.05),
-      dt_(2.0e-08),
-      rho_(7952.),            // kg/m^3
-      nu_(0.26),
-      youngs_mod_(193.e9)    // Pa
-{ /* everything done via initialization */ }
+      num_particles_(num_particles)
+{ /* everything else set in hpp file */ }
 
 // our static method to actually instantiate this singleton object
 GlobalSettings& GlobalSettings::getInstance(const int num_particles, const double mean_rad, 
--- src/global_settings.hpp
+++ src/global_settings.hpp
@@ -27,26 +27,26 @@ class GlobalSettings {
     
     public:
         // domain dimensions
-        const double length_;
-        const double width_;
-        const double height_;
+        const double length_ = 0.5e-03; // m
+        const double width_ = 1.0e-03;
+        const double height_ = 3.0e-03;
 
         // particle size distribution
         const double mean_rad_;
         const double stdev_rad_;
-        const double min_rad_;
-        const double max_rad_;
+        const double min_rad_ = 4.0e-06;    
+        const double max_rad_ = 23.0e-06;
         const int num_particles_;
 
         // simulation controls
-        const double t_start_;
-        const double t_end_;
-        const double dt_;
+        const double t_start_ = 0.0; // s
+        const double t_end_ = 0.5;
+        const double dt_ = 2.0e-08;
 
         // material properties
-        const double rho_;
-        const double nu_;
-        const double youngs_mod_;
+        const double rho_ = 7952.0; // kg/m^3
+        const double nu_ = 0.26;    
+        const double youngs_mod_ = 193.0e09; // Pa   
 
         // declare constructor
         static GlobalSettings& getInstance(const int num_particles, const double mean_rad, 
