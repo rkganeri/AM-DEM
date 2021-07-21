9aaef9f8a22816b53fce89a57a28d6f504c17d55
Rishi Ganeriwala <Rishi@Rishis-MacBook.local>
2021-07-20 20:20:05 -0700

    Add skeleton routines for powder deposition time stepping

---
+++ src/deposit_powder.cpp
@@ -0,0 +1,39 @@
+#include <string>  
+#include <memory>
+#include <cmath>
+
+#include "Kokkos_Core.hpp"
+#include "fmt/core.h"
+
+#include "deposit_powder.hpp"
+#include "io_utils.hpp"
+#include "terminate.hpp"
+
+namespace amdem {
+
+bool depositPowder(std::unique_ptr<Particles>& particles, std::unique_ptr<Bins>& bins,
+                   const GlobalSettings& global_settings) {
+
+    // we are using an explicit RK-4 time stepping scheme, so our step size is fixed
+    double current_time = global_settings.start_time_;
+    const double end_time = global_settings.end_time_;
+    const double dt = global_settings.dt_;
+    const int num_time_steps = static_cast<int>(ceil((end_time-current_time) / dt));
+
+    // create initial plot state
+    plotState(particles, current_time, 0);
+
+    for (int istep=1; istep<=num_time_steps; istep++) {
+
+
+
+    }
+
+
+
+}
+
+} // namespace amdem
+
+
+
---
+++ src/deposit_powder.hpp
@@ -0,0 +1,22 @@
+#ifndef AMDEM_DEPOSIT_POWDER_HPP
+#define AMDEM_DEPOSIT_POWDER_HPP
+
+#include <string>  
+#include <memory>
+
+#include "global_settings.hpp"
+#include "particles.hpp"
+#include "bins.hpp"
+
+namespace amdem {
+
+bool depositPowder(std::unique_ptr<Particles>& particles, std::unique_ptr<Bins>& bins,
+                   const GlobalSettings& global_settings);
+
+} // namespace amdem
+
+#endif // AMDEM_DEPOSIT_POWDER_HPP
+
+
+
+
