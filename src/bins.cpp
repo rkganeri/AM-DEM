2a5a8cd8e8e2387c1693de0f65c48437135320c6
Rishi Ganeriwala <Rishi@Rishis-MacBook.lan1>
2021-07-29 18:58:03 -0500

    delete num_particles from bins class since we no longer need it

--- src/bins.cpp
+++ src/bins.cpp
@@ -10,16 +10,15 @@ Bins::Bins(const GlobalSettings& gs)
     : num_bins_x_(static_cast<int>(gs.length_/(4*gs.max_rad_))),
       num_bins_y_(static_cast<int>(gs.width_/(4*gs.max_rad_))),
       num_bins_z_(static_cast<int>(gs.height_/(4*gs.max_rad_))),
-      num_particles_(gs.num_particles_),
       particle_bin_("particle_bin",gs.num_particles_),
       bins_("bins",num_bins_x_,num_bins_y_,num_bins_z_),
-      linked_list_("linked_list",num_particles_)
+      linked_list_("linked_list",gs.num_particles_)
 { /* everything done via initialization */ }
 
 void Bins::initBins() {
     // Kokkos views are initialized to 0 by default but we really need to set the bins_
     // and linked_list_ arrays to -1
-    Kokkos::parallel_for("init_linked_list", num_particles_, KOKKOS_LAMBDA(int i) {
+    Kokkos::parallel_for("init_linked_list", linked_list_.extent(0), KOKKOS_LAMBDA(int i) {
         linked_list_(i) = -1;
     });
 
--- src/bins.hpp
+++ src/bins.hpp
@@ -17,7 +17,6 @@ class Bins {
         const int num_bins_x_;
         const int num_bins_y_;
         const int num_bins_z_;
-        const int num_particles_;
 
         Kokkos::View<int*[3]> particle_bin_; // (num_particles,3)
         Kokkos::View<int***> bins_;          // (num_bins_x,num_bins_y,num_bins_z)
